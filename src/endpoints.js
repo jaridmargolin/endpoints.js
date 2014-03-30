a2022c87acfd5c8b0247c39b45ee8d4975c29599
Jarid Margolin <jaridmargolin@gmail.com>
2014-03-30 16:59:27 -0700

    Add decorators option & add configure method

--- src/endpoints.js
+++ src/endpoints.js
@@ -5,8 +5,26 @@
  */
 
 define([
+  'utils',
   'resource',
-], function (Resource) {
+], function (_u_, Resource) {
+
+
+// ----------------------------------------------------------------------------
+// Endpoints defaults
+// ----------------------------------------------------------------------------
+
+// Basic
+var defaults = {
+  dataType : 'json',
+  timeout  : 5000,
+  headers  : {}
+};
+
+// Headers
+defaults.headers = {
+  'Content-Type': 'application/json'
+};
 
 
 // ----------------------------------------------------------------------------
@@ -17,18 +35,40 @@ define([
 // - Normalizes interface for passing data to various methods (GET, POST, DEL)
 // - Provides defaults for api.
 // ----------------------------------------------------------------------------
-var Endpoints = function (ajax, opts) {
-  for (var key in opts.resources) {
-    this[key] = new Resource(opts.resources[key], opts, ajax);
-  }
 
+var Endpoints = function (ajax, opts) {
+  var decorators = opts.decorators || {},
+      resources  = opts.resources;
+
+  // Remove all properties that are not defaults
+  // *opts is now equal to defaults.
+  delete opts.decorators;
   delete opts.resources;
-  this.defaults = opts;
+
+  // Mixin module defaults with user specified defaults
+  // in order to create resourceDefaults.
+  this.resourceDefaults = _u_.deepExtend({}, defaults, opts);
+
+  // Loop over resources and create endpoints. Resources are mixed into
+  // the Endpoints instance for easy access.
+  for (var key in resources) {
+    this[key] = new Resource(ajax, this.resourceDefaults, decorators, resources[key]);
+  }
 };
+
+//
+// Configure is a proxy to set properties on default. Behind
+// the scenes it uses a deepMerge.
+//
+Endpoints.prototype.configure = function (data) {
+  _u_.deepMerge(this.resourceDefaults, data);
+};
+
 
 // ----------------------------------------------------------------------------
 // Expose
 // ----------------------------------------------------------------------------
+
 return Endpoints;
 
 
